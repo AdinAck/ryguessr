cc1b9d0442c504c66c15b2af041c1f0b2a53b978
AdinAck <adinackerman@gmail.com>
2026-02-11 20:02:04 -0800

    server: basic API setup (#2)

--- ryguessr/src/main.rs
+++ ryguessr/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+use std::env;
+
+use anyhow::Context;
+use dotenvy::dotenv;
+use google_maps::Client;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenv().ok();
+
+    let api_key =
+        env::var("GOOGLE_MAPS_API_KEY").context("GOOGLE_MAPS_API_KEY must be set in .env")?;
+
+    let _client = Client::try_new(api_key)?;
+
     // more to come...
+
+    Ok(())
 }
