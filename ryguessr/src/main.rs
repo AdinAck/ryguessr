f3dac33f64309f03d0fd4a75eda096bd0e691b0a
Adin Ackerman <adinackerman@gmail.com>
2026-02-11 20:09:32 -0800

    server: get random streetview coordinate

--- ryguessr/src/main.rs
+++ ryguessr/src/main.rs
@@ -1,19 +1,4 @@
-use std::env;
-
-use anyhow::Context;
-use dotenvy::dotenv;
-use google_maps::Client;
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    dotenv().ok();
-
-    let api_key =
-        env::var("GOOGLE_MAPS_API_KEY").context("GOOGLE_MAPS_API_KEY must be set in .env")?;
-
-    let _client = Client::try_new(api_key)?;
-
-    // more to come...
-
-    Ok(())
+    todo!()
 }
