83fac7dff9eb9b8514f71641f3cd374e886cc1ef
Canyon <118639113+Canyon-C@users.noreply.github.com>
2026-02-12 21:53:01 -0800

    Streetview component checkpoint (#7)
    Removed the iframe; now using the react-google-maps/api component

--- web/web-ryguessr/app/page.tsx
+++ web/web-ryguessr/app/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 import { Suspense } from "react"
-import Google_Map from "./ui/sv_map/google-map"
+import Streetview from "./ui/sv_map/google-map"
 export default function Landing() {
   return (
     <main className="m-0 p-0">
       <Suspense>
-        <Google_Map></Google_Map>
+        <Streetview></Streetview>
       </Suspense>
     </main>
   )
--- web/web-ryguessr/app/ui/sv_map/google-map.tsx
+++ web/web-ryguessr/app/ui/sv_map/google-map.tsx
@@ -1,34 +1,34 @@
+"use client"
 import React, { useEffect } from 'react';
 import { APIProvider, Map } from '@vis.gl/react-google-maps';
-const Google_Map = () => {
+import { GoogleMap, StreetViewPanorama, useLoadScript } from "@react-google-maps/api";
+const Streetview = () => {
+  const { isLoaded } = useLoadScript({ googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "" });
 
-  const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
-  const streetview_url = `https://www.google.com/maps/embed/v1/streetview?key=${apiKey}&location=46.414382,10.013988&heading=210&pitch=10&fov=35`
-
-  if (!apiKey) {
-    return <div>Error: Google Maps API key is missing.</div>;
+  if (!isLoaded) {
+    return <div>Loading.... UwU</div>;
   }
-  useEffect(() => {
-    console.log("hello world")
-    console.log(streetview_url);
-  }, [])
+
+  const defaultPosition = { lat: 35.6586, lng: 139.7454 };
 
   return (
-    <iframe
-      src={streetview_url}
-      className="fixed inset-0 w-screen h-screen border-none z-50"
-      referrerPolicy="no-referrer-when-downgrade"
-    />
-    // <APIProvider apiKey={apiKey}>
-    //   <Map
-    //     style={{ width: '100vw', height: '100vh' }}
-    //     defaultCenter={{ lat: 22.54992, lng: 0 }}
-    //     defaultZoom={3}
-    //     gestureHandling='greedy'
-    //     disableDefaultUI
-    //   />
-    // </APIProvider>
+    <GoogleMap
+      zoom={10}
+      mapContainerStyle={{ width: "100vw", height: "100vh" }}
+    >
+      <StreetViewPanorama
+        options={{
+          visible: true,
+          position: defaultPosition,
+          addressControl: false,
+          enableCloseButton: false,
+          fullscreenControl: false,
+          panControl: false,
+          zoomControl: false
+        }}
+      />
+    </GoogleMap>
   );
 };
 
-export default Google_Map;
+export default Streetview;
