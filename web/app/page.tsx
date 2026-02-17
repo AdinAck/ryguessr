d24feaba74b64d205c043a6943d9bb4cbf183ecd
canyon <canyoncavicke@gmail.com>
2026-02-17 02:06:57 -0800

    coordinate fetch location change

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,23 +1,15 @@
-"use client"
-
-import { useEffect, useState } from "react"
 import { GameView } from "@/components/game/GameView"
+import { Suspense } from "react"
 
 
 export default function Landing() {
-  const [initialLocation, setInitialLocation] = useState(null)
 
-  useEffect(() => {
-    fetch("/api/random-location")
-      .then((res) => res.json())
-      .then(setInitialLocation)
-  }, [])
-
-  if (!initialLocation) return null
 
   return (
     <main className="m-0 p-0">
-      <GameView initialLocation={initialLocation} />
+      <Suspense>
+        <GameView />
+      </Suspense>
     </main>
   )
 }
--- web/types/coordinate_type.ts
+++ web/types/coordinate_type.ts
@@ -1,4 +1,4 @@
-export type coordinates = {
+export type Location = {
   lat: number,
   lng: number,
 };
