f9caf6e9e9d399ddb81242e5209f2ffabfb463b3
canyon <canyoncavicke@gmail.com>
2026-03-09 00:52:49 -0700

    score animation

--- web/components/game/Scoreboard.tsx
+++ web/components/game/Scoreboard.tsx
@@ -1,21 +1,37 @@
-import { Table, TableHead, TableBody, TableCaption, TableCell, TableHeader, TableRow } from "../ui/table";
+"use client"
+import NumberFlow from "@number-flow/react";
 import ScoreData from "@/types/score-data";
 import { memo } from "react";
+import { CSSProperties, useState, useEffect } from "react";
 
 export const Scoreboard = memo(({ score_data }: { score_data: ScoreData }) => {
+
+  const [animateToRealScore, setAnimateToRealScore] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAnimateToRealScore(true);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div className="flex flex-col gap-3 w-[50vw] h-full max-h-[40vh] overflow-y-auto pr-2">
       {score_data.player_scores.map((score, index) => {
         return (
           <div
-            className="flex justify-between items-center w-full bg-black/80 border-2 rounded-lg p-2 text-white shadow-md transition-all"
+            key={score.name} className="flex justify-between items-center w-full bg-black/80 border-2 rounded-lg p-2 text-white shadow-md"
           >
             <span className="text-sm">{score.name}</span>
-            <span className="text-right text-sm">{score.cum_score}</span>
+            <NumberFlow
+              className="text-right text-sm font-semibold"
+              value={animateToRealScore ? score.cum_score : ((score.cum_score - score.last_score) || 0)}
+              style={{ '--number-flow-mask-height': '0.3em' } as CSSProperties}
+            />
           </div>
         );
-      })};
+      })}
 
     </div>
 
