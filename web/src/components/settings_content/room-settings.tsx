b4271cd9feeb3526dad34b1e4a436fea2d42fae0
Canyon Cavicke <canyoncavicke@gmail.com>
2026-05-29 12:40:02 -0700

    Room joining checkpoint

--- web/src/components/settings_content/room-settings.tsx
+++ web/src/components/settings_content/room-settings.tsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 import { Input } from "@/components/ui/input";
 
 // Zustand useRoomSettings
-import { useRoomSettings } from "@/store/useSettingsStore";
+import { RoomSettingsActions, useRoomSettings } from "@/store/useSettingsStore";
 // import { RoomSettingsActions } from "@/store/useSettingsStore";
 
 // Error Code enum
@@ -19,9 +19,7 @@ import PlayerData from "@/types/player-data";
 
 const RoomSettings = () => {
   const roomCode = useRoomSettings((state) => state.roomCode);
-  const [roomCodeInput, setRoomCodeInput] = useState<string | undefined>(
-    roomCode,
-  );
+  const [roomCodeInput, setRoomCodeInput] = useState<string>(roomCode);
   const [requestStatusCode, setRequestStatusCode] = useState<StatusCodes>(
     StatusCodes.OK,
   );
@@ -46,42 +44,78 @@ const RoomSettings = () => {
     }
   };
 
-  const tryRoomCode = (roomCode: string) => {
-    setRoomCodeInput(roomCode);
-    if (roomCode.length == 4) {
-      roomPeek(roomCode);
+  const handleRoomJoinRequest = async (requestedRoomCode: string) => {
+    try {
+      const response = await fetch("api/join", {
+        method: "POST",
+        body: JSON.stringify(requestedRoomCode),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      setRequestStatusCode(response.status);
+      if (!response.ok) {
+        throw new Error(`Room join request failed status: ${response.status}`);
+      }
+      const playerData: PlayerData[] = await response.json();
+      RoomSettingsActions.updateRoomCode(requestedRoomCode);
+      setPlayerData(playerData);
+      setRoomCodeInput(requestedRoomCode);
+    } catch (erorr) {
+      throw new Error("Network error while attempting to join a room");
+    } finally {
     }
   };
 
+  const tryRoomCode = (roomCodeCapture: string) => {
+    setRoomCodeInput(roomCodeCapture);
+    if (roomCodeCapture.length == 4 && roomCodeCapture != roomCode) {
+      roomPeek(roomCodeCapture);
+    }
+    // else if (roomCodeCapture.length == 3) {
+    //   setPlayerData([]);
+    // }
+  };
+
+  const handleRoomCodeSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const submittedRoomCode = formData.get("room_code") as string;
+    handleRoomJoinRequest(submittedRoomCode);
+  };
   return (
     <>
       <p className="text-xl font-semibold">Room</p>
       <Separator />
-
-      <Input
-        className={`not-focus:text-7xl tracking-wider border-none select-none bg-black focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-7xl placeholder:text-white focus:text-7xl font-semibold h-fit max-w-full uppercase placeholder:tracking-wider 
+      <form onSubmit={(e) => handleRoomCodeSubmit(e)}>
+        <Input
+          className={`not-focus:text-7xl tracking-wider border-none select-none bg-black focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-7xl placeholder:text-white focus:text-7xl font-semibold h-fit max-w-full uppercase placeholder:tracking-wider 
         ${
-          roomCodeInput && roomCodeInput?.length <= 3
-            ? `text-gray-600`
-            : roomCodeInput?.length == 4 && requestStatusCode == StatusCodes.OK
-              ? `text-amber-500`
-              : roomCodeInput?.length == 4 &&
-                  (StatusCodes.NOT_FOUND || StatusCodes.UNAUTHORIZED)
-                ? `text-red-500`
-                : undefined
+          roomCodeInput == roomCode
+            ? "text-green-500"
+            : roomCodeInput && roomCodeInput.length <= 3
+              ? `text-gray-600`
+              : roomCodeInput.length == 4 && requestStatusCode == StatusCodes.OK
+                ? `text-amber-400`
+                : roomCodeInput.length == 4 &&
+                    (StatusCodes.NOT_FOUND || StatusCodes.UNAUTHORIZED)
+                  ? `text-red-400`
+                  : undefined
         }`}
-        id="input-field-roomcode"
-        name="room_code"
-        type="text"
-        maxLength={4}
-        defaultValue={roomCodeInput}
-        value={roomCodeInput}
-        onBlur={() => {
-          setRoomCodeInput(roomCode);
-          setRequestStatusCode(StatusCodes.OK);
-        }}
-        onChange={(e) => tryRoomCode(e.target.value)}
-      />
+          id="input-field-roomcode"
+          name="room_code"
+          type="text"
+          maxLength={4}
+          defaultValue={roomCodeInput}
+          value={roomCodeInput}
+          enterKeyHint="enter"
+          onBlur={() => {
+            setRoomCodeInput(roomCode);
+            setRequestStatusCode(StatusCodes.OK);
+          }}
+          onChange={(e) => tryRoomCode(e.target.value)}
+        />
+      </form>
 
       <Separator />
       <form className="flex flex-col gap-3 w-full">
@@ -92,38 +126,42 @@ const RoomSettings = () => {
         </Field>
         <Field orientation="horizontal">
           <Button type="submit" className="w-full">
-            New Room
+            Inactive
           </Button>
         </Field>
       </form>
       <Separator />
-      <p className="text-lg font-medium">Players</p>
-      <Separator />
-      <div className="flex flex-col w-full">
-        <Item variant="outline">
-          <ItemContent className="flex flex-row justify-between">
-            {playerData &&
-              playerData.map((player: PlayerData) => {
-                return (
-                  <>
-                    <div
-                      key={player.username}
-                      className="flex flex-row gap-3 text-xl items-center"
-                    >
-                      <MapPin
-                        color={player.color}
-                        strokeWidth={1.5}
-                        size="1em"
-                      />
-                      <ItemTitle>{player.username}</ItemTitle>
-                    </div>
-                    <p className="text-gray-300">{player.score}</p>
-                  </>
-                );
-              })}
-          </ItemContent>
-        </Item>
-      </div>
+      {playerData.length > 0 && roomCodeInput.length == 4 && (
+        <>
+          <p className="text-lg font-medium">Players</p>
+          <Separator />
+          <div className="flex flex-col w-full">
+            <Item variant="outline">
+              <ItemContent className="flex flex-row justify-between">
+                {playerData &&
+                  playerData.map((player: PlayerData) => {
+                    return (
+                      <>
+                        <div
+                          key={player.username}
+                          className="flex flex-row gap-3 text-xl items-center"
+                        >
+                          <MapPin
+                            color={player.color}
+                            strokeWidth={1.5}
+                            size="1em"
+                          />
+                          <ItemTitle>{player.username}</ItemTitle>
+                        </div>
+                        <p className="text-gray-300">{player.score}</p>
+                      </>
+                    );
+                  })}
+              </ItemContent>
+            </Item>
+          </div>
+        </>
+      )}
     </>
   );
 };
