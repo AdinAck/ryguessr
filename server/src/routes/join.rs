b8c6a292ba5e0f9624d1268ebb3c531433991de4
Ryder Casazza <rydercasazza@gmail.com>
2026-05-30 11:22:59 -0700

    return JoinData instead of tuple

--- server/src/routes/join.rs
+++ server/src/routes/join.rs
@@ -1,16 +1,26 @@
 use axum::http::StatusCode;
 use axum::{Json, extract::State};
+use serde::Serialize;
 
 use crate::event::{PlayerData, RoundStartData};
 use crate::{Context, handle, room};
+
+#[derive(Serialize)]
+pub struct JoinResponse {
+    player_data: Vec<PlayerData>,
+    round_data: RoundStartData,
+}
 
 #[tracing::instrument(skip_all, fields(client_id = %*client_id))]
 pub async fn join_handler(
     State(context): State<Context>,
     client_id: handle::Id,
     Json(room_id): Json<room::Id>,
-) -> Result<Json<(Vec<PlayerData>, RoundStartData)>, StatusCode> {
+) -> Result<Json<JoinResponse>, StatusCode> {
     let (players, round_data) = context.move_client_to_room(&client_id, &room_id).await?;
 
-    Ok(Json((players, round_data)))
+    Ok(Json(JoinResponse {
+        player_data: players,
+        round_data,
+    }))
 }
